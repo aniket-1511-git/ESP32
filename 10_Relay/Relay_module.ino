431f9106dfd20559ae1dd6d4daecaea78ba88eaf
aniket omanna <aniket.omanna15@gmail.com>
2025-08-04 21:38:07 +0530

    add 10_relay

---
+++ 10_Relay/Relay_module.ino
@@ -0,0 +1,14 @@
+#define relay_u8 25
+void setup()
+{
+pinMode(relay_u8 ,OUTPUT);
+
+}
+void loop()
+{
+	digitalWrite((relay_u8 ,HIGH);
+	delay(1000);
+	digitalWrite((relay_u8 ,LOW);
+	delay(1000);
+	
+}
